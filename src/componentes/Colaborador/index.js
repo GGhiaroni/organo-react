b9ec4ae32eece44bc9eb3067c28e6f2805d3c288
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-14 08:32:28 -0300

    inserindo colaboradores apenas nos seus respectivos times

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,17 +1,14 @@
 import "./Colaborador.css";
 
-const Colaborador = (props) => {
+const Colaborador = ({ nome, imagem, cargo }) => {
   return (
     <div className="colaborador">
       <div className="cabecalho">
-        <img
-          src="https://www.usmagazine.com/wp-content/uploads/2018/03/g-eazy-zoom.jpg?crop=0px%2C0px%2C2000px%2C1130px&resize=1200%2C675&quality=55&strip=all"
-          alt="foto de perfil do colaborador"
-        />
+        <img src={imagem} alt={nome} />
       </div>
       <div className="rodape">
-        <h4>G-Eazy</h4>
-        <h5>Rapper Gay</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   );
